92ab17b44e00dbee1375e5f36f58ed0046481b38
“PedroFabian43“ <pmfabiangarcia@gmail.com>
2026-02-20 20:23:14 +0100

    he creado tablas

--- webinformacion/formularios/urls.py
+++ webinformacion/formularios/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     path('saludo/', views.saludo, name='saludo'),
     path('sumar/', views.sumarnumeros, name='sumar'),
     path('parimpar/', views.Parimpar, name='parimpar'),
-    path('collatz/', views.Collatz, name='collatz')
+    path('collatz/', views.Collatz, name='collatz'),
+    path('tabla/', views.tablaMultiplicar, name='tabla'),
+    path('deportes/', views.deportes, name="deporte"),
+    path('colores/', views.colores, name="color"),
+    path('comics/', views.comics, name='comic'),
 ]
--- webinformacion/formularios/views.py
+++ webinformacion/formularios/views.py
@@ -72,3 +72,127 @@ def Collatz(request):
     else:
         #Sin datos
         return render(request, "paginas/collatz.html")
+    
+def tablaMultiplicar(request):
+    if('cajanumero' in request.POST):
+        #Tenemos el dato cogido
+        listamultiplicacion = []
+        numero = int(request.POST['cajanumero'])
+        maximo = int(request.POST['cajamulti'])
+        for i in range(maximo):
+            i = i + 1
+            multi = numero * i
+            operacion = f"{numero} * {i}"
+            listamultiplicacion.append(
+                {
+                    "operacion":operacion,
+                    "resultado":multi,
+                }
+            )
+        informacion = {
+            "listanumeros":listamultiplicacion
+        }
+        return render(request, "paginas/tablas.html", informacion)
+    else:
+        return render(request, "paginas/tablas.html")
+    
+def deportes(request):
+    listaDeportes = ["Futbol", "Petanca", "Curling", "Dardos", "Parchis", "Rana", "Voleyball", "Baloncesto"]
+    if('selectdeporte' in request.POST):
+        deporte = request.POST['selectdeporte']
+        informacion = {
+            "listadeportes": listaDeportes,
+            "deporte": deporte
+        }
+        return render(request, "paginas/deportes.html", informacion)
+    else:
+        informacion = {
+            "listadeportes": listaDeportes
+        }
+        return render(request, "paginas/deportes.html", informacion)
+    
+def colores(request):
+    listaColores = [
+        {
+            "color":"Rojo",
+            "valor":"red"
+        },
+        {
+            "color":"Verde",
+            "valor":"green"
+        },
+        {
+            "color":"Azul",
+            "valor":"blue"
+        },
+        {
+            "color":"Amarillo",
+            "valor":"yellow"
+        },
+        {
+            "color":"Morado",
+            "valor":"purple"
+        },
+        {
+            "color":"Rosa",
+            "valor":"pink"
+        },
+        {
+            "color":"Gris",
+            "valor":"grey"
+        },
+    ]
+    if('selectcolor' in request.POST):
+        color = request.POST['selectcolor']
+        informacion = {
+            "listacolores":listaColores,
+            "color":color
+        }
+        return render(request, "paginas/colores.html", informacion)
+    else:
+        informacion = {
+            "listacolores": listaColores
+        }
+        return render(request, "paginas/colores.html", informacion)
+    
+def comics(request):
+    listaComics = [
+        {
+            "index": 0,
+            "titulo": "Spiderman",
+            "imagen": "https://elcoleccionistacomics.com/60266-medium_default/spiderman-de-todd-mcfarlane-vol1-de-6.jpg"
+        },
+        {
+            "index": 1,
+            "titulo": "Spawn",
+            "imagen": "https://m.media-amazon.com/images/I/91hZO1pjAoL._AC_UF1000,1000_QL80_.jpg"
+        },
+        {
+            "index": 2,
+            "titulo": "Wolverine",
+            "imagen": "https://www.kamekame.es/53918-large_default/marvel-omnibus-dinastia-de-x-potencias-de-x-comic.jpg"
+        },
+        {
+            "index": 3,
+            "titulo": "Wolverine",
+            "imagen": "https://www.kamekame.es/53918-large_default/marvel-omnibus-dinastia-de-x-potencias-de-x-comic.jpg"
+        },
+        {
+            "index": 4,
+            "titulo": "Asterix y Obelix",
+            "imagen": "https://comicsbarcelona.com/wp-content/uploads/2015/11/127913-Asterix-2.-La-Hoz-de-Oro.jpg"
+        } 
+    ]
+    if ('selectcomic' in request.POST):
+        indice = int(request.POST['selectcomic'])
+        comic = listaComics[indice]
+        informacion = {
+            "listacomics":listaComics,
+            "comic":comic
+        }
+        return render(request, "paginas/comics.html", informacion)
+    else:
+        informacion = {
+            "listacomics":listaComics,
+        }
+        return render(request, "paginas/comics.html", informacion)
